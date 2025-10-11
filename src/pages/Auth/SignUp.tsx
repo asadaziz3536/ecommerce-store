45df31f4b265ac9d3a23422478ba1aa4c1848f55
asadaziz3536 <asadaziz3536@gmail.com>
2025-10-11 07:29:28 +0500

    signup form validation

--- src/pages/Auth/SignUp.tsx
+++ src/pages/Auth/SignUp.tsx
@@ -11,56 +11,87 @@ import { toast } from "react-toastify";
 const SignUp = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [terms, setTerms] = useState(false);
 
-  const [errors, setErrors]=useState({
-    email:'',
-    password:'',
-    conditions:''
-  })
+  const [errors, setErrors] = useState({
+    email: "",
+    password: "",
+    terms: "",
+  });
 
   const handleChange = (e) => {
-    const { name, value } = e.target;
+    const { name, value, checked } = e.target;
 
     if (name === "email") {
       setEmail(value);
-      if(email.trim()===""){
-        setErrors({...errors, email:"Email is required"})
-      }
-      else if(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(value)){
-setErrors({...errors, email:"Email is Invalid"})
-      }
-      else{
-        setErrors({...errors, email:"Email is required"})
+      if (email.trim() === "") {
+        setErrors({ ...errors, email: "Email is required" });
+      } else if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(value)) {
+        setErrors({ ...errors, email: "Email is Invalid" });
+      } else {
+        setErrors({ ...errors, email: "" });
       }
     }
     if (name === "password") {
       setPassword(value);
+
+      if (value.trim() === "") {
+        setErrors((prev) => ({ ...prev, password: "Password is required" }));
+      } else if (
+        !/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm.test(value)
+      ) {
+        setErrors((prev) => ({
+          ...prev,
+          password:
+            "Password must have at least 8 characters, one uppercase, one lowercase, and one number.",
+        }));
+      } else {
+        setErrors((prev) => ({ ...prev, password: "" }));
+      }
+    }
+    if (name === "terms") {
+      setTerms(checked);
+      if (checked) {
+        setErrors((prev) => ({ ...prev, terms: "" }));
+      }
     }
   };
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if(!email){
-      setErrors({...errors, email:"Email is required"})
+    if (!email) {
+      setErrors({ ...errors, email: "Email is required" });
+      return 
     }
-   
+    if (!password) {
+      setErrors((prev) => ({ ...prev, password: "Password is required" }));
+      return
+    }
+    if (!terms) {
+      setErrors((prev) => ({
+        ...prev,
+        terms: "Agree to terms and conditions",
+      }));
+      return
+    }
+
     try {
       const userCredential = await createUserWithEmailAndPassword(
-          auth,
+        auth,
         email,
-        password,
+        password
       );
       const user = userCredential.user;
 
-    console.log("user created", user)
+      console.log("user created", user);
 
-    if(user){
-      toast.success("user created successfully!");
-      setEmail("");
-      setPassword("");
-    }
+      if (user) {
+        toast.success("user created successfully!");
+        setEmail("");
+        setPassword("");
+      }
     } catch (error) {
-      console.log(error.message)
+      console.log(error.message);
     }
   };
   return (
@@ -103,15 +134,24 @@ setErrors({...errors, email:"Email is Invalid"})
               value={password}
               onChange={handleChange}
             />
+            <p className="text-red-500">{errors.password}</p>
           </div>
 
-          <div className="flex justify-between">
+          <div className="flex flex-col justify-between">
             <div className="flex gap-2 items-center">
-              <Checkbox  />
+              {/* <Checkbox /> */}
+
+              <input
+                type="checkbox"
+                name="terms"
+                id=""
+                onChange={handleChange}
+              />
               <label htmlFor="">
                 I agree to the <b>Terms & conditions</b>
               </label>
             </div>
+            <p className="text-red-500">{errors.terms}</p>
           </div>
         </Form>
       </div>
